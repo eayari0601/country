6faff24bf7e886a02c2587ea57f7555a5ab308ef
ikramayari <ikram.ayari123@gmail.com>
2025-11-23 16:35:42 +0100

    : Provider implementation with loading states

---
+++ lib/providers/country_provider.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import '../models/country.dart';
+import '../services/country_service.dart';
+
+class CountryProvider with ChangeNotifier {
+  List<Country> _countries = [];
+  bool _isLoading = false;
+  String _error = '';
+
+  List<Country> get countries => _countries;
+  bool get isLoading => _isLoading;
+  String get error => _error;
+  bool get hasError => _error.isNotEmpty;
+
+  CountryProvider() {
+    loadCountries();
+  }
+
+  Future<void> loadCountries() async {
+    _isLoading = true;
+    notifyListeners(); 
+
+    try {
+      _countries = await CountryService().getAllCountries();
+      _error = '';
+    } catch (e) {
+      _error = 'Failed to load countries';
+      _countries = []; 
+    }
+
+    _isLoading = false;
+    notifyListeners(); 
+  }
+}
