fff7c8f668158076d79185c0a9a23d8c9afc0ef0
ikramayari <ikram.ayari123@gmail.com>
2025-11-23 17:13:11 +0100

    test api

--- lib/services/country_service.dart
+++ lib/services/country_service.dart
@@ -3,20 +3,23 @@ import 'package:http/http.dart' as http;
 import '../models/country.dart';
 
 class CountryService {
- 
-  
   Future<List<Country>> getAllCountries() async {
     try {
-     
       final response = await http.get(
-        Uri.parse('https://www.apicountries.com/'),
+        Uri.parse('https://www.apicountries.com/api/countries'),
       );
 
       if (response.statusCode == 200) {
-        final List<dynamic> data = json.decode(response.body);
-        return data.map((json) => Country.fromJson(json)).toList();
+        final Map<String, dynamic> data = json.decode(response.body);
+        final List<dynamic> countriesData = data['data'];
+        
+        List<Country> countries = [];
+        for (var countryData in countriesData) {
+          countries.add(Country.fromJson(countryData));
+        }
+        return countries;
       } else {
-        throw Exception('Failed to load countries: ${response.statusCode}');
+        throw Exception('Failed to load countries');
       }
     } catch (e) {
       throw Exception('Failed to load countries: $e');
